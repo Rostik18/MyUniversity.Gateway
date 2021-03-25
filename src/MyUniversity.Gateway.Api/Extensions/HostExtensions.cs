82de51badd71c863055dc984b7533a53ea1bb9b0
RostyslavBaitsar <rostik1804@gmail.com>
2021-03-25 14:57:17 +0200

    one more clean up

--- src/MyUniversity.Gateway.Api/Extensions/HostExtensions.cs
+++ src/MyUniversity.Gateway.Api/Extensions/HostExtensions.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using MyUniversity.Gateway.Api.MapperProfiles;
+using MyUniversity.Gateway.Api.Settings;
 
 namespace MyUniversity.Gateway.Api.Extensions
 {
